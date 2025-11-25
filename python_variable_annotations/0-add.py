01576d9b728ec44576e535bd07d291bffc4c63c5
kevin10v <vokakevin@icloud.com>
2025-11-25 10:32:04 +0100

    update

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Function that adds two floats."""
+
+
+def add(a: float, b: float) -> float:
+    """Add two floats and return the sum.
+    
+    Args:
+        a: First float number
+        b: Second float number
+    
+    Returns:
+        Sum of a and b as float
+    """
+    return a + b
