0f9f53402ff742c28400c97d5ef460d7e99b6e5b
kevin10v <vokakevin@icloud.com>
2025-11-25 10:43:58 +0100

    update

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Function that concatenates two strings."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenate two strings and return the result.
+
+    Args:
+        str1: First string
+        str2: Second string
+
+    Returns:
+        Concatenated string
+    """
+    return str1 + str2
