97ce63fce2e77da52f15c2558c64ce19e9929c5a
kevin10v <vokakevin@icloud.com>
2025-11-26 11:49:06 +0100

    update

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """Async Generator module"""
 import asyncio
 import random
-from typing import Generator
+from typing import AsyncGenerator
 
 
-async def async_generator() -> Generator[float, None, None]:
+async def async_generator() -> AsyncGenerator[float, None]:
     """Coroutine that yields 10 random numbers between 0 and 10"""
     for _ in range(10):
         await asyncio.sleep(1)
--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -8,11 +8,6 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 async def measure_runtime() -> float:
     """Executes async_comprehension 4 times in parallel and measures runtime"""
-    start_time = time.time()
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
-    return time.time() - start_time
+    start_time = time.perf_counter()
+    await asyncio.gather(*[async_comprehension() for _ in range(4)])
+    return time.perf_counter() - start_time
