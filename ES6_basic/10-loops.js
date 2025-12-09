ad2d99caaaa30b9631424f6102a5892c6c7e2085
kevin10v <vokakevin@icloud.com>
2025-12-09 14:21:07 +0100

    Add ES6 Basics project with all 13 tasks implemented
    Implemented all ES6 fundamental concepts including:
    - const/let vs var (task 0-1)
    - Arrow functions and default parameters (task 2-3)
    - Rest and spread operators (task 4-5)
    - Template literals and object shorthand (task 6-7)
    - Computed properties and ES6 methods (task 8-9)
    - for...of loops and iterators (task 10-12)
    
    All tasks tested and pass ESLint validation. Includes test files for verification.

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const newArray = [];
   for (const value of array) {
-    newArray.push(appendString + value);
+    const idx = array.indexOf(value);
+    array[idx] = appendString + value;
   }
 
-  return newArray;
+  return array;
 }
