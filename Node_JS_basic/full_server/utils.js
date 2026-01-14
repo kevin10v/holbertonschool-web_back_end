b0a39eab7907d50f455740c3ad9191e55c5b8676
kevin10v <vokakevin@icloud.com>
2026-01-14 13:37:57 +0100

    Fix: ESLint indentation and line length in StudentsController

---
+++ Node_JS_basic/full_server/utils.js
@@ -0,0 +1,28 @@
+import fs from 'fs';
+
+export default function readDatabase(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
+      const students = lines.slice(1);
+
+      const fields = {};
+      students.forEach((line) => {
+        const [firstname, , , field] = line.split(',');
+        if (field) {
+          if (!fields[field]) {
+            fields[field] = [];
+          }
+          fields[field].push(firstname);
+        }
+      });
+
+      resolve(fields);
+    });
+  });
+}
